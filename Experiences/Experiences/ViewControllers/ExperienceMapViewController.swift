20d899e4c519e516119f5ff01e4e102cbeb8aaa7
Fabiola <saga.fabiola@gmail.com>
2020-01-27 13:03:53 -0600

    Added ExperienceMapViewController and connected it to MapView on storyboard

---
+++ Experiences/Experiences/ViewControllers/ExperienceMapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ExperienceMapViewController.swift
+//  Experiences
+//
+//  Created by Fabiola S on 1/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ExperienceMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
